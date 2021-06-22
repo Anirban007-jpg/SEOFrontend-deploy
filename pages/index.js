b3385acfb676314ed395ae874445a764c82f816c
Anirban Banerjee <abanerjee763@outlook.com>
2021-06-22 15:55:09 +0530

    First commit 2

--- pages/index.js
+++ pages/index.js
@@ -77,29 +77,6 @@ const Index = () => {
                             </div>
                         </div>
 
-                        <div className="col-md-4">
-                            <div className="flip flip-horizontal">
-                                <div
-                                    className="front"
-                                    style={{
-                                        backgroundImage:
-                                            'url(' +
-                                            'https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg' +
-                                            ')'
-                                    }}
-                                >
-                                    <h2 className="text-shadow text-center h1">Next</h2>
-                                </div>
-                                <div className="back text-center">
-                                    <Link href="/categories/nextjs">
-                                        <a>
-                                            <h3 className="h1">Next Js</h3>
-                                        </a>
-                                    </Link>
-                                    <p className="lead">A Production ready web framework for building SEO React apps</p>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </article>
