5337adbd104f02ea90982cf87996deb0e699666d
Anirban Banerjee <abanerjee763@outlook.com>
2021-06-22 12:03:06 +0530

    First commit 2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 const withCSS = require('@zeit/next-css');
 
-module.exports = withCSS({
-    publicRuntimeConfig: {
-        APP_NAME: 'SEOBLOG',
-        API_DEVELOPMENT: 'http://localhost:8000/api',
-        PRODUCTION: true,
-        DOMAIN_DEVELOPMENT: 'http://localhost:3000',
-        DISQUS_SHORTNAME: 'seoblog-ucno9dvq83',
-        GOOGLE_CLIENT_ID: '76720414364-e8auk97b1h9q8d9o5ck58ohiug61ddj1.apps.googleusercontent.com'
-    }
-})
+module.exports = withCSS({})
