f5e3899998dee05797a762b6e2e6c15328d660ea
Anirban Banerjee <abanerjee763@outlook.com>
2021-06-22 12:01:24 +0530

    First commit 2

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-import getConfig from 'next/config'
-const {publicRuntimeConfig} = getConfig()
 
-export const API = publicRuntimeConfig.PRODUCTION ? 'https://seomultiblog-backend.herokuapp.com/api' : 'http://localhost:8000/api';
-export const API_NAME = publicRuntimeConfig.APP_NAME;
-export const DOMAIN = publicRuntimeConfig.PRODUCTION ? 'https://seomultiblog-backend.herokuapp.com' : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
+
+
+export const API = 'https://seomultiblog-backend.herokuapp.com/api';
+export const API_NAME = "SEOBLOG";
+export const DOMAIN = 'https://seomultiblog-backend.herokuapp.com'
 export const DISQUS_SHORTNAME = 'seoblog-ucno9dvq83';
-export const GOOGLE_CLIENT_ID = publicRuntimeConfig.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_ID = '76720414364-e8auk97b1h9q8d9o5ck58ohiug61ddj1.apps.googleusercontent.com';
