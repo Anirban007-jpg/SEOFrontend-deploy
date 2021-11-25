e67f6e761c6b7c6d84312c36e82b65de13b84399
Anirban Banerjee <65713021+Anirban007-jpg@users.noreply.github.com>
2021-11-25 22:43:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 
 export const API = 'https://seomultiblog-backend.herokuapp.com/api';
-export const API_NAME = "SEOBLOG";
+export const API_NAME = "MAFFILIATE";
 export const DOMAIN = 'https://seomultiblog-backend.herokuapp.com'
 export const DISQUS_SHORTNAME = 'seoblog-ucno9dvq83';
 export const GOOGLE_CLIENT_ID = '76720414364-e8auk97b1h9q8d9o5ck58ohiug61ddj1.apps.googleusercontent.com';
