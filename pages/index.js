a7065b7112177e1c5984d1d70ec4445bcb813979
Anirban Banerjee <65713021+Anirban007-jpg@users.noreply.github.com>
2022-04-02 23:06:48 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -25,31 +25,7 @@ const Index = () => {
                         </div>
                     </div>
                 </div>
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <div className="flip flip-horizontal">
-                                <div
-                                    className="front"
-                                    style={{
-                                        backgroundImage:
-                                            'url(' +
-                                            'https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg' +
-                                            ')'
-                                    }}
-                                >
-                                    <h2 className="text-shadow text-center h1">React</h2>
-                                </div>
-                                <div className="back text-center">
-                                    <Link href="/categories/react">
-                                        <a>
-                                            <h3 className="h1">React Js</h3>
-                                        </a>
-                                    </Link>
-                                    <p className="lead">The world's most popular frontend web development library</p>
-                                </div>
-                            </div>
-                        </div>
+           
 
                       
 
