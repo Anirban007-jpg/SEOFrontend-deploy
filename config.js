ff619fae9b4a29583922cf92cba01513589bbce7
Anirban Banerjee <65713021+Anirban007-jpg@users.noreply.github.com>
2024-01-15 20:46:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 
 
-
-export const API = 'https://seomultiblog-backend.herokuapp.com/api';
+export const DOMAIN = 'https://seoback-n7dw.onrender.com'
+export const API = `${DOMAIN}/api`;
 export const API_NAME = "MAFFILIATE";
-export const DOMAIN = 'https://seomultiblog-backend.herokuapp.com'
 export const DISQUS_SHORTNAME = 'seoblog-ucno9dvq83';
 export const GOOGLE_CLIENT_ID = '76720414364-e8auk97b1h9q8d9o5ck58ohiug61ddj1.apps.googleusercontent.com';
